c0dd660dbf252527b82565b17922eabfcd92e04d
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-22 19:57:34 +0700

    - MENYELESAIKAN HOME PAGE ROOT -
    *). Menambahkan section lagi yaitu untuk menampilkan spesifikasi data yaitu data timeline dan market quotes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
 import TradingViewWidget from "@/components/TradingViewWidget";
-import {MARKET_OVERVIEW_WIDGET_CONFIG} from "@/lib/constants";
+import {
+    HEATMAP_WIDGET_CONFIG,
+    MARKET_DATA_WIDGET_CONFIG,
+    MARKET_OVERVIEW_WIDGET_CONFIG,
+    TOP_STORIES_WIDGET_CONFIG
+} from "@/lib/constants";
 
 const Home = () => {
     // scriptUrl : https://s3.tradingview.com/external-embedding/**kondsional(market-overview.js/widget-mini-symbol-overview.js/dll)**
@@ -25,6 +30,22 @@ const Home = () => {
                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
                        className="custom-chart"
                        height={600}
+                   />
+               </div>
+           </section>
+            <section className="grid w-full gap-8 home-section">
+               <div className="w-full md:col-span-1 xl:col-span-1">
+                   <TradingViewWidget
+                       scriptUrl={`${scriptUrl}timeline.js`} // timeline
+                       config={TOP_STORIES_WIDGET_CONFIG}
+                       className="custom-chart"
+                       height={600}
+                   />
+               </div>
+               <div className="w-full md:col-span-1 xl:col-span-2">
+                   <TradingViewWidget
+                       scriptUrl={`${scriptUrl}market-quotes.js`} // market-quotes
+                       config={MARKET_DATA_WIDGET_CONFIG}
                    />
                </div>
            </section>
